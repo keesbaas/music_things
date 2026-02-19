0ee0ad958824fdee85da9b678e5d40bb25b75d7e
keesbaas <kbaas3@gmail.com>
2026-02-19 12:30:00 -0700

    Add refresh token handling for Spotify API

--- spotify_update.py
+++ spotify_update.py
@@ -5,16 +5,21 @@ from spotipy.oauth2 import SpotifyOAuth
 
 CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+REFRESH_TOKEN = os.environ.get("SPOTIFY_REFRESH_TOKEN")
 REDIRECT_URI = "http://localhost:8888/callback"
 
 scope = "user-read-recently-played"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+auth_manager = SpotifyOAuth(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
     redirect_uri=REDIRECT_URI,
     scope=scope
-))
+)
+
+token_info = auth_manager.refresh_access_token(REFRESH_TOKEN)
+
+sp = spotipy.Spotify(auth=token_info['access_token'])
 
 results = sp.current_user_recently_played(limit=50)
 
